b5b363514abbc4af23525c7b197c17d5b1788257
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-02-21 21:24:56 -0700

    that's enough for today lol

--- symlinks.sh
+++ symlinks.sh
@@ -2,3 +2,5 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.config/kitty/ ~/.config/kitty/
 ln -sf ~/dotfiles/.config/sway/ ~/.config/sway/
+ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
+ln -sf ~/dotfiles/.Xresources ~/.Xresources
