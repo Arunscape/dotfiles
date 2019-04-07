8abb29d8c747553e880883f1e6bd604b5640d2e3
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-04-07 00:14:15 -0600

    I did some more stuff

--- symlinks.sh
+++ symlinks.sh
@@ -4,3 +4,15 @@ ln -sf ~/dotfiles/.config/kitty/ ~/.config/
 ln -sf ~/dotfiles/.config/sway/ ~/.config/
 ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.Xresources ~/.Xresources
+ln -sf ~/dotfiles/.config/waybar ~/.config
+
+# DNS Servers
+cat > /etc/resolv.conf << EOF
+nameserver 1.1.1.1
+nameserver 1.0.0.1
+nameserver 2606:4700:4700::1111
+nameserver 2606:4700:4700::1001
+EOF
+
+# Make sure that connman won't overwrite this
+chattr +i /etc/resolv.conf
