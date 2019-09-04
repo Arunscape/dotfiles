34269667ef146c809b6c370500dd0ff0640cf62f
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-09-03 22:58:54 -0600

    I'm done today

--- symlinks.sh
+++ symlinks.sh
@@ -5,6 +5,7 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.config/kitty/ ~/.config/
 ln -sf ~/dotfiles/.config/sway/ ~/.config/
+ln -sf ~/dotfiles/.config/i3blocks/ ~/.config/
 #ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 #ln -sf ~/dotfiles/.Xresources ~/.Xresources
 #ln -sf ~/dotfiles/.config/waybar ~/.config/
