eb4b758c0555faa4b2d86252ddc876db0a88e60f
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-10-05 15:12:09 -0600

    stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,11 @@ thefuck --alias | source
 if test (tty) = "/dev/tty1"
     exec sway
 end
+
+set PATH $PATH (yarn global bin)
+# set PATH $PATH $HOME/.local/bin
+set PATH $PATH $HOME/.cargo/bin
+
+set -gx _JAVA_AWT_WM_NONREPARENTING 1
+set -g fish_emoji_width 2
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
