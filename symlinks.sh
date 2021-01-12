99eecf3c350f424f14e8dbe936e2ad114aa5e296
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-01-12 15:43:39 -0700

    add nvidia-xrun config

--- symlinks.sh
+++ symlinks.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir -p $HOME/.config 
+mkdir -p $HOME/.config
+mkdir $HOME/.config/X11
 mkdir -p $HOME/.vim 
 ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
 ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/dotfiles/.makepkg.conf $HOME/.makepkg.conf
 ln -sf $HOME/dotfiles/.latexmkrc $HOME/.latexmkrc
-
 ln -sf $HOME/dotfiles/.config/kitty/ $HOME/.config/
 ln -sf $HOME/dotfiles/.config/sway/ $HOME/.config/
 ln -sf $HOME/dotfiles/.config/fish/ $HOME/.config/
@@ -24,5 +24,6 @@ ln -sf $HOME/dotfiles/.config/base16-shell $HOME/.config/base16-shell
 ln -sf $HOME/dotfiles/.minecraft/minecraft.sh $HOME/.minecraft/minecraft.sh
 ln -sf $HOME/dotfiles/.doom.d $HOME/.doom.d
 ln -sf $HOME/dotfiles/.emacs.d $HOME/.emacs.d
+ln -sf $HOME/dotfiles/.config/X11/nvidia-xinitrc $HOME/.config/X11/nvidia-xinitrc
 
 sudo ln -sf /bin/dash /bin/sh
