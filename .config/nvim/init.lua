8655442bf616d4b37cb1b12ffdc0c874fc9baaeb
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-06-30 16:46:36 -0600

    changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -55,7 +55,7 @@ vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_li
 --nnoremap <silent> <leader>f :Format<CR>
 
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ignore_install = { }, -- List of parsers to ignore installing
   highlight = {
     enable = true,              -- false will disable the whole extension
