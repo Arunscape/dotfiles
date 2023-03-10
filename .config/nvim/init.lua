1fb2e38ad03efa5db4ecf6f8efe7f4e2743b3bd1
Arunscape <8227297+Arunscape@users.noreply.github.com>
2023-03-10 00:28:42 -0700

    formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -34,13 +34,13 @@ vim.opt.autoindent = true
 vim.opt.copyindent = true
 vim.opt.smartindent = true
 
-vim.opt.wildignore = {'*/cache/*', '*/tmp/*', "*/target/*", "*/build/*", "*/node_modules/*"}
+vim.opt.wildignore = { '*/cache/*', '*/tmp/*', "*/target/*", "*/build/*", "*/node_modules/*" }
 
 --vim.api.nvim_set_keymap(mode, key, result, {noremap = true, silent = true})
 -- leader+y to copy to system clipboard
-vim.api.nvim_set_keymap("", "<leader>y", [["+y]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap("", "<leader>y", [["+y]], { noremap = true, silent = true })
 -- leader+p to paste from system clipboard
-vim.api.nvim_set_keymap("", "<leader>p", [["+p]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap("", "<leader>p", [["+p]], { noremap = true, silent = true })
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -55,30 +55,30 @@ require('plugins')
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-    automatic_installation = true,
-    automatic_setup = true,
+  automatic_installation = true,
+  automatic_setup = true,
 })
 
 local coq = require "coq"
 require("mason-lspconfig").setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function (server_name) -- default handler (optional)
-            require("lspconfig")[server_name].setup {
-              coq.lsp_ensure_capabilities{}
-            }
-        end,
-        -- Next, you can provide a dedicated handler for specific servers.
-        -- For example, a handler override for the `rust_analyzer`:
-        -- ["rust_analyzer"] = function ()
-        --     require("rust-tools").setup {}
-        -- end
+  -- The first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  function(server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup {
+      coq.lsp_ensure_capabilities {}
+    }
+  end,
+  -- Next, you can provide a dedicated handler for specific servers.
+  -- For example, a handler override for the `rust_analyzer`:
+  -- ["rust_analyzer"] = function ()
+  --     require("rust-tools").setup {}
+  -- end
 }
 
 require("mason-null-ls").setup({
-    automatic_installation = true,
-    automatic_setup = true,
+  automatic_installation = true,
+  automatic_setup = true,
 })
 require('mason-null-ls').setup_handlers()
 
@@ -92,34 +92,34 @@ require('mason-null-ls').setup_handlers()
 --vim.cmd('colorscheme carbonfox')
 
 require("catppuccin").setup({
-    flavour = "mocha",
-    transparent_background = true,
-    integrations = {
-      coc_nvim = true,
-      treesitter = true,
-      indent_blankline = {
-        enabled = true,
-        colored_indent_levels = true,
-      },
-      markdown = true,
-      mason = true,
-      nvimtree = true,
-      telescope = true,
-      gitsigns = true,
-    }
+  flavour = "mocha",
+  transparent_background = true,
+  integrations = {
+    coc_nvim = true,
+    treesitter = true,
+    indent_blankline = {
+      enabled = true,
+      colored_indent_levels = true,
+    },
+    markdown = true,
+    mason = true,
+    nvimtree = true,
+    telescope = true,
+    gitsigns = true,
+  }
 })
 vim.cmd.colorscheme "catppuccin"
 
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = {'org'},
+    additional_vim_regex_highlighting = { 'org' },
   },
 }
 
-require('telescope').setup{}
+require('telescope').setup {}
 require('telescope').load_extension('fzf')
 
 require("nvim-tree").setup({
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,8 +1,8 @@
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -37,25 +37,25 @@ return require('packer').startup(function(use)
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     -- or                       , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
   use {
     'nvim-treesitter/nvim-treesitter',
     run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-          end,
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
   }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use {
     'nvim-tree/nvim-tree.lua',
     requires = { 'nvim-tree/nvim-web-devicons' },
   }
   use 'lewis6991/gitsigns.nvim'
   use "lukas-reineke/indent-blankline.nvim"
-  use {'nvim-orgmode/orgmode', config = function()
-      require('orgmode').setup{}
-    end
+  use { 'nvim-orgmode/orgmode', config = function()
+    require('orgmode').setup {}
+  end
   }
 
   -- Automatically set up your configuration after cloning packer.nvim
@@ -64,4 +64,3 @@ return require('packer').startup(function(use)
     require('packer').sync()
   end
 end)
-
