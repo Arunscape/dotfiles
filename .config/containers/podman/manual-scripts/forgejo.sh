0f27415f2ced9a20378efb1b994f2668659a80db
Arunscape <8227297+Arunscape@users.noreply.github.com>
2025-07-29 23:45:32 -0600

    add deemix rc

--- .config/containers/podman/manual-scripts/forgejo.sh
+++
@@ -1,9 +0,0 @@
-podman run --name forgejo \
--p 3000:3000 \
--p 2222:2222 \
--v /run/media/arunscape/znas/server/forgejo/data:/var/lib/gitea \
--v /run/media/arunscape/znas/server/forgejo/conf:/etc/gitea \
---userns=keep-id \
---restart=always \
---cgroup-manager=cgroupfs --replace \
-codeberg.org/forgejo/forgejo:10-rootless
