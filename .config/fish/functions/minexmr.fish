39185994e74217b5d59a2cc9966a64aedbd64224
Arunscape <8227297+Arunscape@users.noreply.github.com>
2026-06-17 00:14:35 -0600

    changes

--- .config/fish/functions/minexmr.fish
+++ .config/fish/functions/minexmr.fish
@@ -1,4 +1,8 @@
 function minexmr
+  set -l threads $argv[1]
+  test -n "$threads"; or set threads (nproc)
+
+  echo "Running with $threads threads"
   # -t 23 means 23 threads
-  doas xmrig -o pool.supportxmr.com:9000 -u 89uzt9WNeXmQYGn6P5CbVo9wo67qm2mULepyXvyjS5UDaHduSDkVtKPiGQo9nTGitCLLQ1F8hg6LYeN99ujv1Gh72HARnPu -k --tls --rig-id desktop --donate-level 0 -t 24 --yield true
+  doas xmrig -o pool.supportxmr.com:9000 -u 89uzt9WNeXmQYGn6P5CbVo9wo67qm2mULepyXvyjS5UDaHduSDkVtKPiGQo9nTGitCLLQ1F8hg6LYeN99ujv1Gh72HARnPu -k --tls --rig-id desktop --donate-level 0 -t $threads
 end
